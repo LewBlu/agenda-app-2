fbf01140b017c46fdd2d8827275a8e088c6caca7
LewBlu <lewis.blundell.development@gmail.com>
2024-06-03 19:33:40 +0100

    Added FontAwesome, remove tag icon added

--- src/app/shared/components/tag-list/tag/tag.component.ts
+++ src/app/shared/components/tag-list/tag/tag.component.ts
@@ -1,14 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { Tag } from '../../../models/tag';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tag',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './tag.component.html',
   styleUrl: './tag.component.css'
 })
 export class TagComponent {
   @Input() tag!: Tag;
+  faXmark = faXmark;
 }
