9fbf2130297b62e299b9b01083070cfd09cf0fcc
LewBlu <lewis.blundell.development@gmail.com>
2024-06-08 10:04:51 +0100

    Added removal of tags from issues

--- src/app/shared/components/tag-list/tag/tag.component.ts
+++ src/app/shared/components/tag-list/tag/tag.component.ts
@@ -3,6 +3,7 @@ import { Tag } from '../../../models/tag';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { IssueService } from '../../../services/issue.service';
 
 @Component({
   selector: 'app-tag',
@@ -14,4 +15,11 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons';
 export class TagComponent {
   @Input() tag!: Tag;
   faXmark = faXmark;
+
+  constructor(private issueService: IssueService){}
+
+  removeTag(event: MouseEvent) {
+    event.stopPropagation();
+    this.issueService.removeTagFromIssue(this.tag.issue_id, this.tag.id);
+  }
 }
--- src/app/shared/services/issue.service.ts
+++ src/app/shared/services/issue.service.ts
@@ -7,12 +7,37 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class IssueService {
-  issues$ = new BehaviorSubject<Issue[]|undefined>(undefined);
+  issues$ = new BehaviorSubject<Issue[]>([]);
   constructor(private httpClient: HttpClient) { }
-
+  
   fetchIssues() {
     this.httpClient.get<Issue[]>('api/issue').subscribe((res: Issue[]) => {
       this.issues$.next(res);
     });
+  }
+
+  updateLocalIssue(issue_id: number, updatedDetails: {}) {
+    const issues = this.issues$.value;
+    const updatedIssues = issues?.map(issue => {
+      if(issue.id != issue_id) {
+        return issue;
+      }
+      let updatedIssue: Issue = {...issue, ...updatedDetails};
+      return updatedIssue;
+    });
+
+    this.issues$.next(updatedIssues);
+  }
+
+  /* -------------------------------------------------------------------------- */
+  /*                                    TAGS                                    */
+  /* -------------------------------------------------------------------------- */
+
+  removeTagFromIssue(issue_id: number, tag_id: number) {
+    const issues = this.issues$.value;
+    let selectedIssue = issues.filter(issue => issue.id == issue_id)[0];
+    selectedIssue.tags = selectedIssue.tags?.filter(tag => tag.id != tag_id);
+
+    this.httpClient.delete(`api/tag/${tag_id}`).subscribe();
   }
 }
